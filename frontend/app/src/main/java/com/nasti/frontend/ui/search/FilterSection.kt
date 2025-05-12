c97ad0c175742148a9c54289b9248f73add386c1
alevchenko12 <nastiya.levchenko@gmail.com>
2025-05-12 22:02:27 +0200

    Booking Checked

--- frontend/app/src/main/java/com/nasti/frontend/ui/search/FilterSection.kt
+++ frontend/app/src/main/java/com/nasti/frontend/ui/search/FilterSection.kt
@@ -7,14 +7,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-<<<<<<< HEAD
 fun FilterSection(
     viewModel: SearchViewModel,
     onApply: () -> Unit // <-- new callback
 ) {
-=======
-fun FilterSection(viewModel: SearchViewModel) {
->>>>>>> 985333f97a20d610b5261bd58ea38b6b72c68e52
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -66,12 +62,8 @@ fun FilterSection(viewModel: SearchViewModel) {
 
         Button(
             onClick = {
-<<<<<<< HEAD
                 viewModel.triggerSearch()
                 onApply() // <-- hide filter panel
-=======
-                viewModel.triggerSearch() // <- You must define this in your ViewModel
->>>>>>> 985333f97a20d610b5261bd58ea38b6b72c68e52
             },
             modifier = Modifier.fillMaxWidth()
         ) {
