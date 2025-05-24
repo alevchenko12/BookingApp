cb86ff368d6624b7ed1c1e8611752b0bc1e35376
Anastasiia Levchenko <nastiya.levchenko@gmail.com>
2025-05-24 18:54:40 +0200

    Run on laptop

--- frontend/app/src/main/java/com/nasti/frontend/data/api/RetrofitClient.kt
+++ frontend/app/src/main/java/com/nasti/frontend/data/api/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8000/"
+    private const val BASE_URL = "http://192.168.1.104:8000/"
 
     // Default client (used for login/register)
     private val retrofit: Retrofit by lazy {
@@ -41,7 +41,7 @@ object RetrofitClient {
     }
 
     object RetrofitClient {
-        private const val BASE_URL = "http://10.0.2.2:8000/"
+        private const val BASE_URL = "http://192.168.1.104:8000/"
 
         private val retrofit: Retrofit by lazy {
             Retrofit.Builder()
